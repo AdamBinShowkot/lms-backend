b1f94cf511d90a03054208e7051b9ae7ba8fd596
AdamBinShokot <adambinshowkot@gmail.com>
2023-07-14 22:41:41 +0600

    Change For Sidebar Menu

--- controllers/menus/get.js
+++ controllers/menus/get.js
@@ -49,7 +49,8 @@ const GetMenusController=async(req,res)=>{
                             Id:mlData.id,
                             ModuleId:mlData.ModuleId,
                             ModuleName:mlData.name,
-                            menus:Menus
+                            IconName:mlData.IconName,
+                            children:Menus
                         }
                     ]
                 },[])
@@ -72,7 +73,7 @@ const GetMenusController=async(req,res)=>{
 
 const GetUserModulePermission = async (UserId) => {
     try {
-        let query=`Select mdp.id , mdp.ModuleId , m.name from 
+        let query=`Select mdp.id,mdp.ModuleId,m.name,m.IconName from 
         modulepermission  mdp
         inner join modules m on mdp.ModuleId=m.id
         where mdp.UserId=${UserId} and mdp.IsActive=1`;
@@ -89,7 +90,7 @@ const GetUserMenuPermissionWithGroupId = async (GroupId,ModuleId,res) => {
    if(GroupId && ModuleId){
         try {
             let query=`Select 
-            pg.id as id , m.ModuleId, pg.MenuId , m.name as name,m.IconName,m.path , pg.IsCreate,pg.IsUpdate,pg.IsReport,pg.IsView
+            pg.id as id , pg.MenuId , m.name as name,m.IconName,m.path , pg.IsCreate,pg.IsUpdate,pg.IsReport,pg.IsView
             from permissiongroup pg
             inner join menus m on pg.MenuId=m.id
             where pg.IsActive=1 AND pg.GroupId=${GroupId} AND m.ModuleId=${ModuleId} AND m.IsActive=1`;
@@ -109,7 +110,7 @@ const GetUserMenuPermissionWithUserId = async (UserId,ModuleId,res) => {
     if(UserId && ModuleId){
         try {
             let query=`Select 
-            pg.id as id , m.ModuleId, pg.MenuId , m.name as name,m.IconName,m.path,pg.IsCreate,pg.IsUpdate,pg.IsReport,pg.IsView
+            pg.id as id ,pg.MenuId , m.name as name,m.IconName,m.path,pg.IsCreate,pg.IsUpdate,pg.IsReport,pg.IsView
             from permissions pg
             inner join menus m on pg.MenuId=m.id
             where pg.IsActive=1 AND pg.UserId=${UserId} AND m.ModuleId=${ModuleId} AND m.IsActive=1`;
